2873424d14422796b6bcf32bb0c89663ff16b3c9
kerorinnf <kerorinnf@gmail.com>
2025-12-11 04:20:46 +0900

    推論結果の確率を取得する実装に変更

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import torch.nn.functional as F
 from torchvision import models
 from train import val_transform
 from tqdm import tqdm
@@ -13,10 +14,15 @@ def predict(image_path, model, class_names, device):
 
     with torch.no_grad():
         outputs = model(x)
-        _, pred = torch.max(outputs, 1)
-        class_id = pred.item()
+        probs = F.softmax(outputs, dim=1)
+        max_prob, pred = torch.max(probs, dim=1)
         
-    return class_names[class_id], class_id
+    max_prob = max_prob.item()
+    class_id = pred.item()
+    if max_prob < 0.7:
+        return "other", -1, max_prob
+        
+    return class_names[class_id], class_id, max_prob
     
 def load_model(model_path, class_names):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -28,11 +34,11 @@ def load_model(model_path, class_names):
 
     return model, device
 
-def display(image_path, class_name, class_id):
+def display(image_path, class_name, class_id, prob):
     img = Image.open(image_path)
     plt.figure(figsize=(10,4))
     plt.imshow(img)
-    plt.title(f"Predicted : {class_name}, id : {class_id}", fontsize=16)
+    plt.title(f"Predicted : {class_name}, id : {class_id}, prob: {prob}", fontsize=16)
     plt.axis("off")
     plt.show()
 
@@ -43,10 +49,10 @@ if __name__ == "__main__":
     image_pathes = sorted(
         [os.path.join(DIR, f) for f in os.listdir(DIR) if f.lower().endswith(("png", "jpg", "jpeg"))]
     )
-    class_names = ["hoshino", "kisaki", "mari"]
+    class_names = ["hoshino", "kisaki", "mari", "seia"]
     model, device = load_model(MODEL, class_names)
 
     for image_path in tqdm(image_pathes):
-        name, id = predict(image_path, model, class_names, device)
-        display(image_path, name, id)
+        name, id, prob = predict(image_path, model, class_names, device)
+        display(image_path, name, id, prob)
 
