229b59e5e5f682f48bed590f2e8c1d3aba043f43
kerorinnf <kerorinnf@gmail.com>
2025-12-14 23:01:02 +0900

    feat: 画像前処理に複数の処理を追加. 結果, 汎化性能が上がったように見える

--- src/train.py
+++ src/train.py
@@ -18,14 +18,17 @@ VAL_RATIO = 0.2
 
 ### 画像の前処理 ###
 train_transform = transforms.Compose([
-    transforms.Resize((256,256)),
-    transforms.RandomCrop((224, 224)),
+    # transforms.Resize((256,256)),
+    # transforms.RandomCrop((224, 224)),
+    transforms.RandomResizedCrop(size=224, scale=(0.7, 1.0), ratio=(0.75, 1.33)), # add
     transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05), # add
     transforms.ToTensor(),
     transforms.Normalize(
         mean=[0.485,0.456,0.406],
         std=[0.229,0.224,0.225],
     ),
+    transforms.RandomErasing(p=0.5, scale=(0.02, 0.15), ratio=(0.3, 3.3), value="random") # add
 ])
 val_transform = transforms.Compose([
     transforms.Resize((224,224)),
